f569f149c5dd6285eb45f6aba2d054c0c9ce8853
maintao <dongchao@outlook.com>
2023-12-24 09:48:30 +0800

    sync

--- sync-npmmirror.js
+++ sync-npmmirror.js
@@ -1,6 +1,6 @@
-import { name } from "./package.json";
+const packageJson = require("./package.json");
 
-fetch(`https://registry-direct.npmmirror.com/-/package/${name}/syncs`, {
+fetch(`https://registry-direct.npmmirror.com/-/package/${packageJson.name}/syncs`, {
   headers: {
     accept: "*/*",
     "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -18,9 +18,9 @@ fetch(`https://registry-direct.npmmirror.com/-/package/${name}/syncs`, {
   body: null,
   method: "PUT",
 })
-  .then((res) => {
+  .then(async (res) => {
     console.log("sync npmmirror success");
-    console.log(res.json());
+    console.log(await res.json());
   })
   .catch((err) => {
     console.error("sync npmmirror error");
